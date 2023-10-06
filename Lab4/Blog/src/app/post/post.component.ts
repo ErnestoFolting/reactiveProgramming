a90a673ce75657209ae4df13d06c34a836936db9
ErnestoFolting <danilsmy.edu@gmail.com>
2023-10-06 03:35:26 +0300

    Add completed task

---
+++ Lab4/Blog/src/app/post/post.component.ts
@@ -0,0 +1,29 @@
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+
+import { Post } from '../app.component';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css'],
+})
+
+export class PostComponent implements OnDestroy {
+  @Input() myPost!: Post;
+  @Output() onRemove = new EventEmitter<number>();
+
+  removePost() {
+    this.onRemove.emit(this.myPost.id);
+  }
+
+  ngOnDestroy() {
+    console.log('метод ngOnDestroy');
+  }
+}
